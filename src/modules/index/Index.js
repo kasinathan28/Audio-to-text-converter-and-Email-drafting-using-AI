a3f7241bed56b74d24f8e7886931fa76cc452b35
Kasinathan <way2kasinathb35@gmail.com>
2024-03-21 09:57:39 +0530

    Index page desgin complete.

--- src/modules/index/Index.js
+++ src/modules/index/Index.js
@@ -1,11 +1,39 @@
-import React from 'react'
+import React from 'react';
+import "./index.css";
+
+// Images
+import Avatar from "./assets/9440461.jpg";
 
 function Index() {
   return (
-    <div>
-      <h1>Index</h1>
+    <div className='container'>
+      <div className='main'>
+        <div className='left'>
+          <div className='list'>
+            <h1>Dev-HUT</h1>
+          </div>
+          <div className='list2'>
+            <div className='user'>
+              <img src={Avatar} alt="User Avatar" className="avatar" />
+              <div className="user-details">
+                <h3>User Name</h3>
+                <p>User Status</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className='right'>
+          <div className='chat'>
+            
+          </div>
+          <div className="chat-input">
+            <input type="text" placeholder="Type a message..." />
+            <button>Send</button>
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Index
+export default Index;
