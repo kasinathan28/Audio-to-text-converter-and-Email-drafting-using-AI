10a16d87f31fd98b99eee584f541a733e8efdef5
Kasinathan <way2kasinathb35@gmail.com>
2024-03-21 22:20:33 +0530

    lOGIN AND SIGNUP PAGE ADDING

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Index from "./modules/index/Index.js";
+import Auth from "./modules/auth/Auth.js";
 
 function App() {
   return (
     <Router>
     <Routes>
       <Route path="/" element ={<Index/>}/>
+      <Route path="/auth" element ={<Auth/>}/>
     </Routes>
   </Router>
   );
---
+++ src/modules/auth/Auth.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import "./auth.css"
+
+function Auth() {
+  return (
+    <div className='authc' >
+      <div className='left1'>
+
+      </div>
+      <div className='right1'>
+
+      </div>
+    </div>
+  )
+}
+
+export default Auth
